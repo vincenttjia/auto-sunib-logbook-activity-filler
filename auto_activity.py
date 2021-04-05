314363f4e9af79594d32f224452221052269076e
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-04-05 22:56:59 +0700

    Initial Commit

---
+++ auto_activity.py
@@ -0,0 +1,142 @@
+import requests
+import json
+
+ssourl = "https://activity-enrichment.apps.binus.ac.id/Auth/SSOStudentLogin?token=DIGANTI DENGAN TOKEN MASING-MASING"
+Activity = "Aws Academy"
+Description = "Aws Academy"
+
+def main():
+  global ssourl,Activity,Description
+
+  s = requests.session()
+  s.get(ssourl)
+  url = "https://activity-enrichment.apps.binus.ac.id/LogBook/StudentSave"
+
+  # INSERT OFF
+  # FEBRUARY
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-02-22T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-02-23T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-02-24T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-02-25T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-02-26T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-02-27T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-02-28T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  #MARET
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-02T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-04T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-06T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-09T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-011T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-12T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-13T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-16T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-18T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-20T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-23T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-27T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-30T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  # APRIL
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-04-01T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-04-02T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-04-03T00:00:00","model[Activity]":"OFF","model[ClockIn]":"OFF","model[ClockOut]":"OFF","model[Description]":"OFF"}
+  r = s.post(url, data=payload)
+  print(r.json())
+
+
+
+  # INSERT Log Book
+  # MARET
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-01T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-03T00:00:00","model[Activity]":Activity,"model[ClockIn]":"09:20 am","model[ClockOut]":"13:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-05T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-08T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-10T00:00:00","model[Activity]":Activity,"model[ClockIn]":"11:20 am","model[ClockOut]":"13:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-15T00:00:00","model[Activity]":Activity,"model[ClockIn]":"01:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-17T00:00:00","model[Activity]":Activity,"model[ClockIn]":"09:20 am","model[ClockOut]":"01:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-19T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-22T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-24T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"05:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-25T00:00:00","model[Activity]":Activity,"model[ClockIn]":"07:20 pm","model[ClockOut]":"09:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-26T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-29T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"07:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-03-31T00:00:00","model[Activity]":Activity,"model[ClockIn]":"09:20 am","model[ClockOut]":"01:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+
+  #APRIL
+  payload = {"model[ID]":"00000000-0000-0000-0000-000000000000","model[LogBookHeaderID]":"002d4297-5962-4ffc-976e-9a28d0f74c43","model[Date]":"2021-04-05T00:00:00","model[Activity]":Activity,"model[ClockIn]":"03:20 pm","model[ClockOut]":"05:00 pm","model[Description]":Description}
+  r = s.post(url, data=payload)
+  print(r.json())
+
+if __name__ == '__main__':
+  main()
